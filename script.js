aa56e10066ba2450f45ba116d20dd89ee0c9fc5a
Richard <moorerichardplease@gmail.com>
2024-04-30 12:10:10 +0900

    add dialogue into HTML file

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function Book() {
 
 
 function addBookToLibrary() {
-    //1. prompt user for information
+    // 1. prompt user (popup dialogue) for information when "add book" button is pushed (title, author, page count, read?(checkbox)). also add confirm and cancel buttons in that popup.
+
+    // 2a. if cancel is pressed, close dialogue and clear info
+    // 2b. if submit is pressed, store info as object using the book constructor
+    
+    // 3. add the new object to the myLibrary[] array.
+    
+    // 4. create a div with class '.card' to display the data prompt user for information
     
 }
