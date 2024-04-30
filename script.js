063656694aadb5f38a510b216e30469a190ab5c0
Richard <moorerichardplease@gmail.com>
2024-04-30 20:48:31 +0900

    add book constructor and event listener

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 // --------INITIAL VARIABLES--------
-const button = document.querySelector('button');
+const addNewBookButton = document.querySelector('.add-that-book');
+const submitButton = document.querySelector('.submit-button');
+const dialog = document.querySelector('#book-dialog');
 
 // Array to hold multiple cards
 const myLibrary = [];
 
 
-function Book() {
-    // the constructor
+function Book(title, author, length, bookType, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.length = length;
+    this.bookType = bookType;
+    this.readStatus = readStatus;
 }
 
 
-function addBookToLibrary() {
-    // 1. prompt user (popup dialogue) for information when "add book" button is pushed (title, author, page count, read?(checkbox)). also add confirm and cancel buttons in that popup.
+addNewBookButton.addEventListener('click', () => {
+    dialog.showModal();
+})
 
-    // 2a. if cancel is pressed, close dialogue and clear info
-    // 2b. if submit is pressed, store info as object using the book constructor
+// handle dialogue
+function displayDialog() {
+    // 1. prompt user (popup dialogue) for information when "add book" button is pushed
+    //Popup the dialog window when the button is pushed.
+    
+    // 2a. if anywhere outside the dialog window is clicked, close dialogue and clear info
+
+
+
+// 2b. if submit is pressed, store info as object using the book constructor
+}
+
+
+
+
+function addBookToLibrary() {
+
+
     
     // 3. add the new object to the myLibrary[] array.
     
