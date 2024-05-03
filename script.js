817fcc43aa78d4edbf7754a3bafe42c872596529
Richard <moorerichardplease@gmail.com>
2024-05-03 15:54:35 +0900

    add logic to get dialog data into card in script document

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ let myLibrary = [];
 
 // this should happen when the submit button is pushed.
 function addBookToLibrary() {
-    //1. need to get value from the forms
+    //1. get value from the forms
     getValuesFromDialog();
 
-    //2. need to push that data into the object
+    //2. push that data into the object
     myLibrary.push(newBook);
     
-    //3. need to loop through said object to put data into card
+    //3. loop through array to put object data into cards
     bookObjectToCardData(newBook);
 }
 
@@ -65,10 +65,71 @@ function getValuesFromDialog() {
     const author = document.querySelector('#author').value;
     const length = document.querySelector('#length').value;
     const readStatus = document.querySelector('#checkboxes').checked;
-    let newBook = new Book(title, author, length, readStatus);
+    const newBook = new Book(title, author, length, readStatus);
     return newBook;
 }
 
-function bookObjectToCardData(book) {
+function bookObjectToCardData(newBook) {
+    //get card-space div
+    const cardSpace = document.querySelector('.card-space');
+
+    //1. begin creating from largest to smallest. 
+
+    //LARGEST (top section of the card)
+    //create card section
+    let card = document.createElement('div');
+    card.setAttribute(`class`, `card`);
+    //create cover section
+    let coverDiv = document.createElement('div');
+    coverDiv.setAttribute(`class`, `cover`);
+    //create div to hold the title
+    let titleDiv = document.createElement('div');
+    titleDiv.setAttribute('class', `title`);    
+    //title
+    let titleH2 = document.createElement('h2');
+    titleH2.textContent = newBook.title;
+    //div to hold the author
+    let authorDiv = document.createElement('div');
+    authorDiv.setAttribute('class', 'author');
+    //author
+    let authorH4 = document.createElement('h4');
+    authorH4.textContent = newBook.author;
+
+    //LARGEST (bottom section of the card)
+    //div to hold info (length and status)
+    let infoDiv = document.createElement('div');
+    infoDiv.setAttribute('class', 'info');
+    //div to hold length
+    let lengthDiv = document.createElement('div');
+    lengthDiv.setAttribute('class', 'length');
+    //length
+    let lengthP = document.createElement('p');
+    lengthP.textContent = newBook.length;
+    //div to hold status
+    let statusDiv = document.createElement('div');
+    statusDiv.setAttribute('class', 'status');
+    //status
+    let statusP = document.createElement('p');
+    statusP.textContent = newBook.readStatus;
+
+    
+    //2. prepend from smallest to largest, bottom to top
+    statusDiv.appendChild(statusP);
+    lengthDiv.appendChild(lengthP);
+    infoDiv.appendChild(lengthDiv);
+    infoDiv.appendChild(statusDiv);
+    authorDiv.appendChild(authorH4);
+    titleDiv.appendChild(titleH2);
+    coverDiv.appendChild(titleDiv);
+    coverDiv.appendChild(authorDiv);
+    card.appendChild(coverDiv);
+    card.appendChild(infoDiv);
+    cardSpace.appendChild(card);
+
+
+    myLibrary.forEach(loopOverObject);
+}
+
+function loopOverObject() {
     
 }
